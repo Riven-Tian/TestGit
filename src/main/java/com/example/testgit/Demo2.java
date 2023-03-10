8ed8a736d314559e0fda2b915669d5a22ea0b8e6
Riven-Tian <tian9577c@163.com>
2023-03-10 11:03:13 +0800

    test2

---
+++ src/main/java/com/example/testgit/Demo2.java
@@ -0,0 +1,15 @@
+package com.example.testgit;
+
+/**
+ * @Author：天儿
+ * @Package：com.example.testgit
+ * @Project：TestGit
+ * @name：Demo2
+ * @Date：2023/3/10 11:00
+ * @Filename：Demo2
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
